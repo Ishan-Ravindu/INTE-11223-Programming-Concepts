e900acb2008c20370f0f2ed9e434485eee2ded26
Ishan Ravindu <ishanravindu27596@gmail.com>
2021-08-18 11:19:48 +0530

    formate resipt section

--- main.cpp
+++ main.cpp
@@ -4,14 +4,15 @@ using namespace std;
 int main(){
 const int allAvailbaleCoinAndNote[11] = {1,2,5,10,20,50,100,500,1000,2000,5000};
 int coinAndNoteCount[11]={0,0,0,0,0,0,0,0,0,0,0};
-int totalBillPrice ;
-int tenderPrice;
+const int maxIndexOfCoinAndNoteArray = sizeof(allAvailbaleCoinAndNote)/sizeof(allAvailbaleCoinAndNote[0]) - 1;
+int total ;
+int cash;
 int balance;
 
 while(true){
 cout <<"Enter Bill Total" <<endl;
-cin >> totalBillPrice;
-if(totalBillPrice>0){
+cin >> total;
+if(total>0){
     break;
 }
 cout<< "Error (enter positive number)" << endl;
@@ -20,8 +21,8 @@ cout<< "Error (enter positive number)" << endl;
 
 while (true){
     cout << "Enter Tender Price"<<endl;
-    cin >> tenderPrice;
-   if(tenderPrice>=totalBillPrice){
+    cin >> cash;
+   if(cash>=total){
        break;
    }else{
        cout<< "Error (enter price must more than or equal to bill total)" << endl;
@@ -29,32 +30,45 @@ while (true){
     
 }
 
-balance = tenderPrice - totalBillPrice;
+balance = cash - total;
 
 if (balance==0){
+    // resipt section 
+    cout<<endl;
     cout << "******BIll******"<<endl;
-    cout <<"Total  = Rs:" <<totalBillPrice <<endl;
-    cout << "Tender = RS:" << tenderPrice <<endl;
+    cout<<endl;
+    cout <<"Total   = Rs:" <<total <<endl;
+    cout << "Cash    = RS:" << cash <<endl;
     cout <<"Balance = Rs:0" <<endl;
+    cout<<endl;
     cout << "***************"<<endl;
+    // end of resipt section 
 }else{
-    int count = sizeof(allAvailbaleCoinAndNote)/sizeof(allAvailbaleCoinAndNote[0]) - 1;
+    int count = maxIndexOfCoinAndNoteArray;
     while(true){
         if(balance == 0 ){
-    
-            cout << coinAndNoteCount[0]<<endl;
-            cout << coinAndNoteCount[1]<<endl;
-            cout << coinAndNoteCount[2]<<endl;
-            cout << coinAndNoteCount[3]<<endl;
-            cout << coinAndNoteCount[4]<<endl;
-            cout << coinAndNoteCount[5]<<endl;
-            cout << coinAndNoteCount[6]<<endl;
-            cout << coinAndNoteCount[7]<<endl;
-            cout << coinAndNoteCount[8]<<endl;
-            cout << coinAndNoteCount[9]<<endl;
-            cout << coinAndNoteCount[10]<<endl;
-            
-            break;
+            // resipt section 
+            cout<<endl;
+            cout << "##############BIll############"<<endl;
+            cout<<endl;
+            cout <<"Total   = Rs:" <<total <<endl;
+            cout << "Cash    = RS:" << cash <<endl;
+            cout <<"Balance = Rs:"<<cash-total <<endl;
+            cout<<endl;
+            cout <<"--------Number Of Coin And Notes--------"<<endl;
+            cout<<endl;
+           for(int i=0;i<maxIndexOfCoinAndNoteArray;i++){
+               if(coinAndNoteCount[i]==0){
+                   continue;
+               }
+                cout<<"Rs:" << allAvailbaleCoinAndNote[i]<<"*";
+                cout << coinAndNoteCount[i]<<endl;
+           }
+             cout<<endl;
+             cout <<"----------------------------------------"<<endl;
+             cout << "################################"<<endl;
+             // end of resipt section 
+             break;
         }else{
             if(balance<allAvailbaleCoinAndNote[count]){
                 count--;
