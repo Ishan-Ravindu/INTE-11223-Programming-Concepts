06c6841e3e69a2d558199bfb82f4150895dfc2d6
Ishan Ravindu <ishanravindu27596@gmail.com>
2021-09-30 19:55:39 +0530

    finish hw08 Q11

--- HomeWork08/Q11.cpp
+++ HomeWork08/Q11.cpp
@@ -3,15 +3,27 @@
 using namespace std;
 
 int main() {
-  char text[100] = "sample text";
+  char text[100] = "sample text";  // only letters
   char* textPtr = &text[0];
+  int vowelsCount = 0;
+  int consonantsCount = 0;
 
   while (true) {
     if (*textPtr == 0) {
       cout << *textPtr << endl;
       break;
-    } else if (*textPtr == ('a' || 'e' || 'i' || 'o' || 'u')) {
-      cout << *textPtr << endl;
+    } else if (*textPtr == 'a' || *textPtr == 'e' || *textPtr == 'i' ||
+               *textPtr == 'o' || *textPtr == 'u') {
+      vowelsCount++;
+
+    } else if (*textPtr == ' ') {
+      textPtr++;
+      continue;
+    } else {
+      consonantsCount++;
     }
+    textPtr++;
   }
+  cout << "vowels Count = " << vowelsCount << endl;
+  cout << "consonants Count = " << consonantsCount << endl;
 }
