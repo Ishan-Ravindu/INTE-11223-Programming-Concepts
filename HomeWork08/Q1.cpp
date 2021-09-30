8e98615100e4dd7775a123eb6e1f5dedec665157
Ishan Ravindu <ishanravindu27596@gmail.com>
2021-09-30 11:43:46 +0530

    finish hw08 Q1

---
+++ HomeWork08/Q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int inputNumber;
+
+  cout << "enter your number =: ";
+  cin >> inputNumber;
+  cout << "input number address is " << &inputNumber << endl;
+}
