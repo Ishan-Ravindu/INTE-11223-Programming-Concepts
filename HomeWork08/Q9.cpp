17cce17b4954cfbda1ac4d6112a93489c4d037d8
Ishan Ravindu <ishanravindu27596@gmail.com>
2021-09-30 17:28:42 +0530

    finish hw08 Q9

---
+++ HomeWork08/Q9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int numberArraySize;
+  cout << "Enter Your number Array Size =:  ";
+  cin >> numberArraySize;
+
+  int numberArray[numberArraySize];
+
+  for (int i = 0; i < numberArraySize; i++) {
+    cout << "Enter number " << i + 1 << "=: ";
+    cin >> numberArray[i];
+  }
+
+  // print array element using pointers
+
+  for (int i = 0; i < numberArraySize; i++) {
+    cout << *(numberArray + i) << endl;
+  }
+}
