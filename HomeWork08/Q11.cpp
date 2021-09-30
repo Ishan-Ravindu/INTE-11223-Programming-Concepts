edb4ce16037fecb76f8df4c767b4fb1d84c3b12a
Ishan Ravindu <ishanravindu27596@gmail.com>
2021-09-30 19:55:05 +0530

    remove unwanted cout

--- HomeWork08/Q11.cpp
+++ HomeWork08/Q11.cpp
@@ -5,15 +5,13 @@ using namespace std;
 int main() {
   char text[100] = "sample text";
   char* textPtr = &text[0];
-  int count = 0;
 
   while (true) {
     if (*textPtr == 0) {
+      cout << *textPtr << endl;
       break;
+    } else if (*textPtr == ('a' || 'e' || 'i' || 'o' || 'u')) {
+      cout << *textPtr << endl;
     }
-    textPtr++;
-    count++;
   }
-
-  cout << "Text size := " << count << endl;
 }
