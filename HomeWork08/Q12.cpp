6ffb71bf40604d0d00264ccf4c03df8127aed367
Ishan Ravindu <ishanravindu27596@gmail.com>
2021-10-02 00:18:47 +0530

    update hw08 Q12 with pointers

--- HomeWork08/Q12.cpp
+++ HomeWork08/Q12.cpp
@@ -2,30 +2,34 @@
 
 using namespace std;
 
+void valueSwap(int*, int*);
+
 int main() {
-  int arrayLength = 10;
-  int numberArray[arrayLength] = {5, 2, 7, 9, 1, 0, 9, 8, 5, 0};
+  int arrayLength = 14;
+  int numberArray[arrayLength] = {62, 5, 2, 7, 9, 1, 0, 9, 8, 5, 0, 1, 2, 58};
   int firstNumberIndex = 0;
   int secondNumberIndex = 1;
+
+  // outer loop section
   while (true) {
     if (firstNumberIndex > arrayLength) {
       for (int i = 0; i < arrayLength; i++) {
-        cout << numberArray[i];
+        cout << *(numberArray + i) << " ";
       }
       cout << endl;
       break;
     } else {
+      // inner loop section
       while (true) {
         if (secondNumberIndex >= arrayLength) {
           firstNumberIndex++;
           secondNumberIndex = firstNumberIndex + 1;
           break;
-        } else if (numberArray[firstNumberIndex] >
-                   numberArray[secondNumberIndex]) {
-          int temNumber1 = numberArray[firstNumberIndex];
-          int tempNumber2 = numberArray[secondNumberIndex];
-          numberArray[firstNumberIndex] = tempNumber2;
-          numberArray[secondNumberIndex] = temNumber1;
+        } else if (*(numberArray + firstNumberIndex) >
+                   *(numberArray + secondNumberIndex)) {
+          valueSwap((numberArray + firstNumberIndex),
+                    (numberArray + secondNumberIndex));
+
           firstNumberIndex = 0;
           secondNumberIndex = 1;
 
@@ -33,6 +37,15 @@ int main() {
           secondNumberIndex++;
         }
       }
+      // end of inner loop section
     }
   }
+  // end of outer loop section
+}
+
+void valueSwap(int* firstNumberPtr, int* secondNumberPtr) {
+  int temValue1 = *(firstNumberPtr);
+  int temValue2 = *(secondNumberPtr);
+  *(firstNumberPtr) = temValue2;
+  *(secondNumberPtr) = temValue1;
 }
